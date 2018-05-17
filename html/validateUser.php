ac290f55b4220f8926b01b18f1c88313c595e28a
DidacR97 <37537956+DidacR97@users.noreply.github.com>
2018-05-17 19:04:22 +0200

    Creado Login Valido en php

---
+++ html/validateUser.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+?>
+
+<?php
+
+	$host_db = "localhost";
+	$user_db = "root";
+	$pass_db = "admin";
+	$db_name = "overhome";
+	$tbl_name = "oh_user";
+
+	$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+
+	if ($conexion->connect_error) {
+	 file_put_contents ("phpLog.txt" , "Error" );// [ $flags = FILE_APPEND [$context ]]
+	}
+
+
+	$nombre= $_POST["uname"];
+	$pass= $_POST["psw"];
+
+	$sql = "SELECT * FROM $tbl_name WHERE NAME = '$nombre' AND PASSWORD ='$pass'";
+
+	$result = $conexion->query($sql);
+
+	if ($result->num_rows > 0) {
+		header('Location: http://35.204.23.49/overhome/html/mainView.html');
+	}
+
+ 	mysqli_close($conexion);
+ ?>
