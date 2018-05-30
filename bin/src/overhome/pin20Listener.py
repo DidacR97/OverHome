ad7700d7a3d325f44631d09c8fbe2ab5f0504baa
spalomos <31198094+spalomos@users.noreply.github.com>
2018-05-30 22:50:01 +0200

    Scripts Rasp

--- bin/src/overhome/pin20Listener.py
+++ bin/src/overhome/pin20Listener.py
@@ -2,16 +2,15 @@ import RPi.GPIO as GPIO
 import time
 import socket
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN)
-# GPIO.setup(18, GPIO.OUT)
+GPIO.setup(20, GPIO.IN)
 UDP_IP = "35.204.23.49"
 UDP_PORT = 12000
-MESSAGE = "00010003;samuel"
+MESSAGE = "00030003;guest,"
 while True:    
-    while True:
-        inputValue = GPIO.input(20)
-        if (inputValue == True):
-            GPIO.output(18, GPIO.HIGH)
-            print("Main_Light Zone 4 ON")
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-            sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+    inputValue = GPIO.input(20)
+    if (inputValue == True):
+        print("action1")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+        time.sleep(1)
+        inputValue = False
--- bin/src/overhome/pin24Listener.py
+++ bin/src/overhome/pin24Listener.py
@@ -3,15 +3,14 @@ import time
 import socket
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN)
-# GPIO.setup(18, GPIO.OUT)
 UDP_IP = "35.204.23.49"
 UDP_PORT = 12000
-MESSAGE = "00010001;samuel"
+MESSAGE = "00030001;guest,"
 while True:    
-    while True:
-        inputValue = GPIO.input(24)
-        if (inputValue == True):
-            GPIO.output(18, GPIO.HIGH)
-            print("Main_Light Zone 4")
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-            sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+    inputValue = GPIO.input(24)
+    if (inputValue == True):
+        print("action1")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+        time.sleep(1)
+        inputValue = False
